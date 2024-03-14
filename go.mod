ae58d337f790bfda6c0b032df4224beead39e25e
comerc <andrew.kachanov@gmail.com>
2024-03-14 23:51:19 +0300

    Add DB to context

--- go.mod
+++ go.mod
@@ -4,21 +4,35 @@ go 1.21.7
 
 require (
 	github.com/99designs/gqlgen v0.17.44
+	github.com/lib/pq v1.10.9
+	github.com/rs/zerolog v1.31.0
+	github.com/uptrace/bun v1.1.17
+	github.com/uptrace/bun/dialect/pgdialect v1.1.17
+	github.com/uptrace/bun/extra/bundebug v1.1.17
 	github.com/vektah/gqlparser/v2 v2.5.11
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sosodev/duration v1.2.0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/urfave/cli/v2 v2.27.1 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/mod v0.14.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- server.go
+++ server.go
@@ -1,28 +1,146 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"log"
 	"net/http"
 	"nordkapp42/graph"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/99designs/gqlgen/graphql/handler"
+	gqlhandler "github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/extra/bundebug"
+
+	_ "github.com/lib/pq"
 )
 
-const defaultPort = "8888"
+const Addr = ":8888"
+const ShutdownTimeout = time.Duration(10) * time.Second
+
+func ForDB(ctx context.Context) *bun.DB {
+	return ctx.Value("db").(*bun.DB)
+}
+
+func WithDB(db *bun.DB) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), "db", db)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func NewAppHandler(db *bun.DB) *http.ServeMux {
+	srv := gqlhandler.NewDefaultServer(
+		graph.NewExecutableSchema(
+			graph.Config{Resolvers: &graph.Resolver{}},
+		),
+	)
+	mux := http.NewServeMux()
+	mux.Handle("/", playground.Handler("GraphQL playground", "/api"))
+	mux.Handle("/api", WithDB(db)(srv))
+	return mux
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
+	// Connect to the database using Bun and the PostgreSQL driver.
+	sqldb, err := sql.Open("postgres", "postgres://postgres:postgrespassword@postgres:5432/postgres?sslmode=disable")
+
+	if err != nil {
+		log.Fatal("open db error", err)
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	// Create a Bun DB instance using the PostgreSQL dialect.
+	db := bun.NewDB(sqldb, pgdialect.New())
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	// Optionally, add a logger to Bun for debugging purposes.
+	db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	defer db.Close()
+
+	// {
+	// 	db := getDB(ctx)
+	// 	var num int
+	// 	err := db.QueryRowContext(context.Background(), "SELECT 1").Scan(&num)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+
+	server := http.Server{
+		Addr:    Addr,
+		Handler: NewAppHandler(db),
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatal("app stopped due error", err)
+		}
+		log.Println("app stopped gracefully")
+	}()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	<-interrupt
+
+	log.Println("app interruption signal received")
+
+	ctx, cancel := context.WithTimeout(context.Background(), ShutdownTimeout)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("app shutdown failed", err)
+	}
+
+	// if err := env.Close(); err != nil {
+	// 	log.Fatal("app environment closing failed", zap.Error(err))
+	// }
+
 }
+
+// func NewGraphQLHandler() *gqlhandler.Server {
+// 	handler := gqlhandler.New(
+// 		runtime.NewExecutableSchema(
+// 			newSchemaConfig(env),
+// 		),
+// 	)
+// 	// Transports
+// 	handler.AddTransport(transport.Websocket{
+// 		KeepAlivePingInterval: websocketKeepAlivePingInterval,
+// 	})
+// 	handler.AddTransport(transport.Options{})
+// 	handler.AddTransport(transport.POST{})
+// 	handler.AddTransport(transport.MultipartForm{
+// 		MaxUploadSize: maxUploadSize,
+// 		MaxMemory:     maxUploadSize / 10,
+// 	})
+// 	// Query cache
+// 	handler.SetQueryCache(lru.New(queryCacheLRUSize))
+// 	// Enabling introspection
+// 	handler.Use(extension.Introspection{})
+// 	// APQ
+// 	handler.Use(extension.AutomaticPersistedQuery{Cache: lru.New(automaticPersistedQueryCacheLRUSize)})
+// 	// Complexity
+// 	handler.Use(extension.FixedComplexityLimit(complexityLimit))
+// 	// Unhandled errors logger
+// 	handler.SetRecoverFunc(func(ctx context.Context, err interface{}) (userMessage error) {
+// 		env.Logger.Error("unhandled error", zap.String("error", fmt.Sprintf("%v", err)))
+// 		return gqlerror.Errorf("internal server error")
+// 	})
+// 	return handler
+// }
+
+// func newSchemaConfig(env *app.Env) runtime.Config {
+// 	cfg := runtime.Config{Resolvers: resolver.NewResolver(env)}
+// 	cfg.Directives.InputUnion = directive.NewInputUnionDirective()
+// 	cfg.Directives.SortRankInput = directive.NewSortRankInputDirective()
+// 	cfg.Complexity.ArticleQuery.Find = resolver.ArticleQueryFindComplexity
+// 	cfg.Complexity.ArticleFindList.TotalCount = resolver.ArticleFindListTotalCountComplexity
+// 	return cfg
+// }
